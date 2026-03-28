00d17406a2e641a74aac60a77598574f155cf0c6
aboutblank0 <josecolaco1999@gmail.com>
2026-03-28 11:50:05 +0000

    added global shortcuts when app is focused

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,22 +1,20 @@
 import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
-import { electronApp, optimizer, is } from '@electron-toolkit/utils'
+import { electronApp, is } from '@electron-toolkit/utils'
+import { OVERLAY_WINDOW_OPTS, OverlayController } from 'electron-overlay-window'
+import { registerShortcuts } from './shortcuts'
 
-function createWindow(): void {
+function createWindow(): BrowserWindow {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 500,
     height: 500,
-    show: false,
     webPreferences: {
       contextIsolation: true,
       sandbox: false,
       preload: join(__dirname, '../preload/index.js')
-    }
-  })
-
-  mainWindow.on('ready-to-show', () => {
-    mainWindow.show()
+    },
+    ...OVERLAY_WINDOW_OPTS
   })
 
   mainWindow.webContents.setWindowOpenHandler((details) => {
@@ -31,6 +29,10 @@ function createWindow(): void {
   } else {
     mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
   }
+
+  OverlayController.attachByTitle(mainWindow, 'Notepad')
+
+  return mainWindow
 }
 
 // This method will be called when Electron has finished
@@ -40,17 +42,11 @@ app.whenReady().then(() => {
   // Set app user model id for windows
   electronApp.setAppUserModelId('com.electron')
 
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-  app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window)
-  })
-
   // IPC test
   ipcMain.on('ping', () => console.log('pong'))
 
-  createWindow()
+  const mainWindow = createWindow()
+  registerShortcuts(mainWindow)
 
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,22 +1,60 @@
-import { useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
+
+type CampaignStep = {
+  id: number
+  instruction: string
+  imageUrl?: string
+}
+
+const steps: CampaignStep[] = [
+  {
+    id: 1,
+    instruction: 'Go over there!'
+  },
+  {
+    id: 2,
+    instruction: 'Okay, now go back to town.'
+  }
+]
 
 function App(): React.JSX.Element {
-  const [randomNumber, setRandomNumber] = useState<number>(0)
+  const [index, setIndex] = useState<number>(0)
+
+  const handleBack = useCallback(() => {
+    setIndex((prev) => Math.max(prev - 1, 0))
+  }, [])
+
+  const handleNext = useCallback(() => {
+    setIndex((prev) => Math.min(prev + 1, steps.length - 1))
+  }, [])
+
+  // Register the events
+  useEffect(() => {
+    const back = window.electron.ipcRenderer.on('back', handleBack)
+    const next = window.electron.ipcRenderer.on('next', handleNext)
+    return () => {
+      back()
+      next()
+    }
+  }, [handleBack, handleNext])
 
   const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 
-  const getRandomNumber = (): void => {
-    const number = window.example.getRandomNumber()
-    setRandomNumber(number)
-  }
-
   return (
     <div>
-      <p>Random Number: {randomNumber}</p>
-      <button onClick={getRandomNumber}>Get Random Number</button>
+      <CampaignStepComponent {...steps[index]} />
       <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
         Send IPC
       </a>
+    </div>
+  )
+}
+
+const CampaignStepComponent = (step: CampaignStep) => {
+  return (
+    <div>
+      <p>ID: {step.id}</p>
+      <h1>{step.instruction}</h1>
     </div>
   )
 }
